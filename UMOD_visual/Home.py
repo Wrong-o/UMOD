af045216ed9ccbf779df26e25f35252bc641dabf
Wrong-o <otto@rongedal.se>
2024-11-14 16:22:43 +0100

    Added better streamlit

---
+++ UMOD_visual/Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.header("Analysis dashboard for INSERT")
+st.subheader("Wordcloud")
+st.write("Visualisation of the questions being asked")
+st.subheader("Usage by customers")
+st.write("Usage metrics of products")
--- UMOD_visual/pages/Wordcloud.py
+++ UMOD_visual/pages/Wordcloud.py
@@ -7,15 +7,13 @@ from nltk.tokenize import word_tokenize
 import streamlit as st
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 from database_manager import DatabaseManager
-nltk.download('punkt')
 
 try:
     db_manager = DatabaseManager()
 except ValueError:
     raise ValueError("Construction of the db_manager failed.")
-
 try:
     db_manager.connect()
 except ConnectionError:
@@ -27,24 +25,25 @@ language_list = db_manager.fetch_languageslist(table="questionlog")
 languages = [item['q_lang'] for item in language_list]
 st.title("NLP of the question asked")
 
-selected_product = st.selectbox("Choose an product:", products)
-selected_language = st.selectbox("Choose an language:", languages)
-word_data = db_manager.fetch_questions_by_language(selected_product, selected_language)
-
-tokens = []
-print(word_data)
-if word_data:
-    print("hit")
-    for entry in word_data:
-        question_text = entry['question']
+selected_product = st.selectbox("Choose a product:", products)
+selected_language = st.selectbox("Choose a language:", languages)
+if st.button("Press to fetch data"):
+    word_data = db_manager.fetch_questions_by_language(selected_product, selected_language)
+    tokens = []
+    if word_data:
+        for entry in word_data:
+            question_text = entry['question']
+            tokens.extend([word.lower() for word in word_tokenize(question_text)])
+    else:
+        question_text = "Missing data Missing data"
         tokens.extend([word.lower() for word in word_tokenize(question_text)])
     text = ' '.join(tokens)
-    wordcloud = WordCloud(width=800, height=400, background_color='black').generate(text)
+    if text:
+        wordcloud = WordCloud(width=800, height=400, background_color='black').generate(text)
+    else:
+        text = ""
     plt.figure(figsize=(10, 5))
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
     st.pyplot(plt)
-else:
-    st.write(f"No question found for {selected_product} in {selected_language}")  
 
-db_manager.close()
